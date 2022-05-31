726e37bf8d0563e1626c99da597fbb20d9860b62
dhawal <thewall4095@gmail.com>
2022-05-31 21:19:16 +0530

    removed unwanted commit

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,11 @@
 import React from "react";
 import "../styles/HomePage.scss";
 import MapContainer from "./../components/pinsmap";
-
 const HomePage = () => {
   return (
     <div id="home-page-container">
-      <div className={"home-page"}>
-        <div>
-
-        </div>
-        <MapContainer />
+      <div className={"home-page "}>
+      <MapContainer />
       </div>
     </div>
   );
